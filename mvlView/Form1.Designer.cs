126b40a1b6e66548051329ef29fb0179c8066e8a
Makise <2929339419@qq.com>
2019-04-15 15:53:23 +0800

    first upload

---
+++ mvlView/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace mvlView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.FileMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openJsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView_eye = new System.Windows.Forms.ListView();
+            this.listView_mouth = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.view = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.FileMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.view)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.FileMenu;
+            this.listBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(118, 228);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // FileMenu
+            // 
+            this.FileMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openJsonToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.FileMenu.Name = "FileMenu";
+            this.FileMenu.Size = new System.Drawing.Size(139, 48);
+            this.FileMenu.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // openJsonToolStripMenuItem
+            // 
+            this.openJsonToolStripMenuItem.Name = "openJsonToolStripMenuItem";
+            this.openJsonToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.openJsonToolStripMenuItem.Text = "Open Json";
+            this.openJsonToolStripMenuItem.Click += new System.EventHandler(this.openJsonToolStripMenuItem_Click);
+            this.openJsonToolStripMenuItem.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listView_eye
+            // 
+            this.listView_eye.Location = new System.Drawing.Point(12, 286);
+            this.listView_eye.Name = "listView_eye";
+            this.listView_eye.Size = new System.Drawing.Size(128, 349);
+            this.listView_eye.TabIndex = 3;
+            this.listView_eye.UseCompatibleStateImageBehavior = false;
+            this.listView_eye.DoubleClick += new System.EventHandler(this.listView_eye_DoubleClick);
+            // 
+            // listView_mouth
+            // 
+            this.listView_mouth.Location = new System.Drawing.Point(146, 286);
+            this.listView_mouth.Name = "listView_mouth";
+            this.listView_mouth.Size = new System.Drawing.Size(131, 349);
+            this.listView_mouth.TabIndex = 4;
+            this.listView_mouth.UseCompatibleStateImageBehavior = false;
+            this.listView_mouth.DoubleClick += new System.EventHandler(this.listView_mouth_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(9, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "bodys";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(9, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "eyes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(143, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "mouths";
+            // 
+            // view
+            // 
+            this.view.Location = new System.Drawing.Point(283, 21);
+            this.view.Name = "view";
+            this.view.Size = new System.Drawing.Size(533, 614);
+            this.view.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.view.TabIndex = 8;
+            this.view.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(280, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "preview";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(172, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(736, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(156, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 53);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Double-click item preview";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(132, 173);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 93);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "By Wetor\r\n(依旧W如此)\r\n2019.4.15\r\nThanks\r\nningshanwutuobang";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 647);
+            this.ContextMenuStrip = this.FileMenu;
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.view);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView_mouth);
+            this.Controls.Add(this.listView_eye);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "mvl View";
+            this.FileMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.view)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ContextMenuStrip FileMenu;
+        private System.Windows.Forms.ToolStripMenuItem openJsonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListView listView_eye;
+        private System.Windows.Forms.ListView listView_mouth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox view;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ mvlView/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace mvlView
+{
+    struct Chara
+    {
+        public string body;
+        public List<string> eyes;
+        public List<string> mouths ;
+    }
+    public partial class Form1 : Form
+    {
+        JObject json;
+        List<Chara> data=new List<Chara>();
+        string path;
+        Bitmap pic;
+        int body_index,eye_index,mouth_index;
+        int body_x0, body_y0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openJsonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Json File|*.json|All File|*.*";
+            file.ShowDialog();
+            if (File.Exists(file.FileName))
+            {
+                data.Clear();
+                listBox1.Items.Clear();
+                path = Path.GetDirectoryName(file.FileName) + "\\";
+                OpenJSON(file.FileName);
+            }
+        }
+
+        public void OpenJSON(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            json = (JObject)JsonConvert.DeserializeObject(sr.ReadToEnd());
+            sr.Close();
+            
+            foreach(var item in json)
+            {
+                if (item.Key.Length==9)
+                {
+                    Chara temp;
+                    temp.body = item.Key;
+                    temp.eyes = new List<string>();
+                    temp.mouths = new List<string>();
+                    foreach (var block in json)
+                    {
+                        if (block.Key.Length == 11 && block.Key.Substring(0,9)== item.Key)
+                        {
+                            if (block.Key.Substring(9, 1) == "E")
+                            {
+                                temp.eyes.Add(block.Key);
+                            }
+                            if (block.Key.Substring(9, 1) == "L")
+                            {
+                                temp.mouths.Add(block.Key);
+                            }
+
+                        }
+                    }
+                    data.Add(temp);
+                }
+            }
+            for(int i = 0; i < data.Count; i++)
+            {
+                listBox1.Items.Add(data[i].body);
+            }
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count <= 0)
+                return;
+
+            body_index = listBox1.SelectedIndex;
+            
+            if (body_index >= 0)
+            {
+                //eyes
+                listView_eye.Clear();
+                ImageList image_list = new ImageList();  
+                for(int i=0;i< data[body_index].eyes.Count; i++)
+                {
+                    image_list.Images.Add(Bitmap.FromFile(path + data[body_index].eyes[i] + ".png"));
+                    image_list.ImageSize = new Size(75, 75);
+                }
+                listView_eye.LargeImageList = image_list;
+                for (int i = 0; i < data[body_index].eyes.Count; i++)
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.ImageIndex = i;
+                    listView_eye.Items.Add(item);
+                }
+
+                //mouths
+                listView_mouth.Clear();
+                image_list = new ImageList();
+                for (int i = 0; i < data[body_index].mouths.Count; i++)
+                {
+                    image_list.Images.Add(Bitmap.FromFile(path + data[body_index].mouths[i] + ".png"));
+                    image_list.ImageSize = new Size(75, 75);
+                }
+                listView_mouth.LargeImageList = image_list;
+                for (int i = 0; i < data[body_index].mouths.Count; i++)
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.ImageIndex = i;
+                    listView_mouth.Items.Add(item);
+                }
+
+
+                //init
+                eye_index = mouth_index = 0;
+                DrawBody(body_index);
+                DrawEye(body_index, eye_index);
+                DrawMouth(body_index, mouth_index);
+                view.Image = pic;
+               
+
+
+            }
+            
+        }
+        public void DrawBody(int body_index, bool save = false)
+        {
+            Bitmap bmp = new Bitmap(path + data[body_index].body + ".png");
+            label5.Text = bmp.Width.ToString() + "x" + bmp.Height.ToString();
+            body_x0 = (int)json[data[body_index].body]["min_x"];
+            body_y0 = (int)json[data[body_index].body]["min_y"];
+            pic = new Bitmap(bmp.Width, bmp.Height);
+            Graphics g = Graphics.FromImage(pic);
+            if(!save)
+                g.Clear(Color.FromArgb(225, 225, 225, 255));
+            g.DrawImage(bmp, 0, 0);
+            bmp.Dispose();
+            g.Dispose();
+        }
+        public void DrawEye(int body_index,int eye_index)
+        {
+            Bitmap bmp = new Bitmap(path + data[body_index].eyes[eye_index] + ".png");
+            Graphics g = Graphics.FromImage(pic);
+            int x = (int)json[data[body_index].eyes[eye_index]]["min_x"] - body_x0;
+            int y = (int)json[data[body_index].eyes[eye_index]]["min_y"] - body_y0;
+            g.DrawImage(bmp, x, y);
+            bmp.Dispose();
+            g.Dispose();
+        }
+        public void DrawMouth(int body_index, int mouth_index)
+        {
+            Bitmap bmp = new Bitmap(path + data[body_index].mouths[mouth_index] + ".png");
+            Graphics g = Graphics.FromImage(pic);
+            int x = (int)json[data[body_index].mouths[mouth_index]]["min_x"] - body_x0;
+            int y = (int)json[data[body_index].mouths[mouth_index]]["min_y"] - body_y0;
+            g.DrawImage(bmp, x, y);
+            bmp.Dispose();
+            g.Dispose();
+        }
+        private void listView_mouth_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView_mouth.SelectedItems.Count <= 0)
+                return;
+            eye_index = listView_mouth.FocusedItem.Index;
+            DrawMouth(body_index, eye_index);
+            view.Image = pic;
+        }
+        private void listView_eye_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView_eye.SelectedItems.Count <= 0)
+                return;
+            mouth_index = listView_eye.FocusedItem.Index;
+            DrawEye(body_index, mouth_index);
+            view.Image = pic;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (body_index < 0 || body_index > data.Count) return;
+            DrawBody(body_index,true);
+            DrawEye(body_index, listView_eye.FocusedItem.Index);
+            DrawMouth(body_index, listView_mouth.FocusedItem.Index);
+            pic.Save(path + data[body_index].body + "E" + eye_index.ToString() + "L" + mouth_index.ToString() + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            MessageBox.Show("Save success!");
+        }
+
+       
+    }
+}
